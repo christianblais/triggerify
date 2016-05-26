65b31ee3365056b037c6bd37cdac4a136cc71899
Christian Blais <christ.blais@gmail.com>
2016-05-26 12:37:13 -0400

    Prevent tagger from retagging same tag

--- app/models/handlers/tagger.rb
+++ app/models/handlers/tagger.rb
@@ -8,6 +8,7 @@ module Handlers
       resource_class = "ShopifyAPI::#{taggable_type}".classify.constantize
       resource = resource_class.find(taggable_id)
       tags = resource.tags.split(',')
+      return if tags.include?(tag_name)
       tags.push(tag_name)
       resource.tags = tags.join(',')
       resource.save
