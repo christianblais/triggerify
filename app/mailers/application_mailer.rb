1c1646f8c6eb3390459c8ebdfdc0a775e1b4cdc5
Christian Blais <christ.blais@gmail.com>
2016-05-26 12:54:55 -0400

    ActionMailer config

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "christ.blais@gmail.com"
   layout 'mailer'
 end
---
+++ config/initializers/action_mailer.rb
@@ -0,0 +1,9 @@
+ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => ENV['MAILGUN_DOMAIN'],
+  :authentication => :plain,
+}
+ActionMailer::Base.delivery_method = :smtp
