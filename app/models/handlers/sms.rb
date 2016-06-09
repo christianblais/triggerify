d98a32c039fff6361d59d810f4ed7ca08f7874fc
Christian Blais <christ.blais@gmail.com>
2016-06-09 13:07:02 -0400

    Allow usage of custom Twilio key

--- app/models/handlers/sms.rb
+++ app/models/handlers/sms.rb
@@ -1,17 +1,20 @@
 module Handlers
   class SMS < Base
+    setting :twilio_account_sid
+    setting :twilio_auth_token
+    setting :twilio_from_phone_number
     setting :phone_number
     setting :message
 
     def call
       return if phone_number.blank? || message.blank?
 
-      client = Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN'])
-      client.account.messages.create(
-        from: ENV['TWILIO_NUMBER'],
-        to: phone_number,
-        body: message
-      )
+      sid = twilio_account_sid.presence || ENV['TWILIO_ACCOUNT_SID']
+      key = twilio_auth_token.presence || ENV['TWILIO_AUTH_TOKEN']
+      nbr = twilio_from_phone_number.presence || ENV['TWILIO_NUMBER']
+
+      client = Twilio::REST::Client.new(sid, key)
+      client.account.messages.create(from: nbr, to: phone_number, body: message)
     rescue Twilio::REST::RequestError => e
       Rails.logger.debug(e)
     end
