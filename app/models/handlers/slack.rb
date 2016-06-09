69d91810f6a05d4f34ff3985ff2b201580e550ce
Christian Blais <christ.blais@gmail.com>
2016-06-09 13:26:48 -0400

    Fix slack constant path

--- app/models/handlers/slack.rb
+++ app/models/handlers/slack.rb
@@ -7,7 +7,7 @@ module Handlers
     def call
       return if channel.blank? || message.blank?
 
-      client = Slack::Web::Client.new(token: slack_token || ENV['SLACK_TOKEN'])
+      client = ::Slack::Web::Client.new(token: slack_token || ENV['SLACK_TOKEN'])
       client.chat_postMessage(channel: channel, text: message, as_user: true)
     rescue Twilio::REST::RequestError => e
       Rails.logger.debug(e)
