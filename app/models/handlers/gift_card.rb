6908c2dc5fd99f5639aa22072c5478fbe2ad1850
Christian Blais <christ.blais@gmail.com>
2016-06-10 11:39:07 -0400

    Fix gift card

--- app/models/handlers/gift_card.rb
+++ app/models/handlers/gift_card.rb
@@ -6,9 +6,8 @@ module Handlers
       return if value.blank?
 
       gift_card = ShopifyAPI::GiftCard.new
-      gift_card.value = value
+      gift_card.initial_value = value
       gift_card.save
-      Rails.logger.info("GIFT_CARD: #{gift_card} : #{gift_card.errors}")
     end
   end
 end
