4fa2f10e89775840cbc89fa0597df6c8ca1768fb
Christian Blais <christ.blais@gmail.com>
2016-06-10 11:36:28 -0400

    Logger around gift cards

--- app/models/handlers/gift_card.rb
+++ app/models/handlers/gift_card.rb
@@ -8,6 +8,7 @@ module Handlers
       gift_card = ShopifyAPI::GiftCard.new
       gift_card.value = value
       gift_card.save
+      Rails.logger.info("GIFT_CARD: #{gift_card} : #{gift_card.errors}")
     end
   end
 end
