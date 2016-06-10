e47da01e882977b87762491677e82c8507a27578
Christian Blais <christ.blais@gmail.com>
2016-06-10 11:43:56 -0400

    Add note and customer to gift card

--- app/models/handlers/gift_card.rb
+++ app/models/handlers/gift_card.rb
@@ -1,12 +1,16 @@
 module Handlers
   class GiftCard < Base
     setting :value
+    setting :note
+    setting :customer_id
 
     def call
       return if value.blank?
 
       gift_card = ShopifyAPI::GiftCard.new
       gift_card.initial_value = value
+      gift_card.note = note
+      gift_card.customer_id = customer_id
       gift_card.save
     end
   end
