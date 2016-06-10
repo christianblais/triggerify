ae48bab2f096fbe6d5d292ed3d1e309d674dc810
Christian Blais <christ.blais@gmail.com>
2016-06-10 11:07:30 -0400

    Fix bug with Filter#verb

--- app/controllers/rules_controller.rb
+++ app/controllers/rules_controller.rb
@@ -48,7 +48,7 @@ class RulesController < ShopifyApp::AuthenticatedController
 
   def rule_params
     rule = params.require(:rule)
-    rule = rule.permit(:name, :topic, handlers_attributes: [:id, :service_name, :_destroy], filters_attributes: [:id, :value, :regex, :_destroy])
+    rule = rule.permit(:name, :topic, handlers_attributes: [:id, :service_name, :_destroy], filters_attributes: [:id, :value, :verb, :regex, :_destroy])
     rule[:handlers_attributes].each do |k, _v|
       rule[:handlers_attributes][k][:settings] = params[:rule][:handlers_attributes][k][:settings]
     end
