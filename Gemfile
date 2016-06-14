d876cef5c926ee27d41f1d634b21bfb05ddfe830
Christian Blais <christ.blais@gmail.com>
2016-06-14 12:34:02 -0400

    Add information and examples to handlers

--- Gemfile
+++ Gemfile
@@ -42,7 +42,7 @@ group :production do
 end
 
 group :development, :test do
-  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  # Call 'byebug' anywhere in the code to stop execution and get a debugger console'
   gem 'byebug'
   gem 'sqlite3'
 end
--- app/models/handlers/emailer.rb
+++ app/models/handlers/emailer.rb
@@ -1,8 +1,16 @@
 module Handlers
   class Emailer < Base
-    setting :recipients
-    setting :subject
-    setting :body
+    setting :recipients,
+      info: 'List of recipient emails',
+      example: 'test@example.com, another@test.com'
+
+    setting :subject,
+      info: 'Title of the email',
+      example: 'Hello {{ first_name }}!'
+
+    setting :body,
+      info: 'Content of the email',
+      example: 'This is an email from triggerify!'
 
     def call
       HandlerMailer.email(to: recipients, subject: subject, body: body).deliver_later
