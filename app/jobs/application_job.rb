6b0ad49457bdeb913022cc527be40842994c9be8
Thierry Joyal <thierry.joyal@gmail.com>
2020-03-24 13:36:05 -0400

    [Bugsnag] Add payload to jobss

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -2,6 +2,8 @@ class ApplicationJob < ActiveJob::Base
   queue_as :default
 
   rescue_from(StandardError) do |exception|
-    Bugsnag.notify(exception)
+    Bugsnag.notify(exception) do |report|
+      report.add_tab(:payload, arguments.first)
+    end
   end
 end
