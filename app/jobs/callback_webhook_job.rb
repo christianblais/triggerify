ac1769fbdd7ead7d581bef63208872b6dd54667a
Thierry Joyal <thierry.joyal@gmail.com>
2020-03-26 07:36:22 -0400

    Callback job track rule specific exceptions

--- app/jobs/callback_webhook_job.rb
+++ app/jobs/callback_webhook_job.rb
@@ -1,13 +1,27 @@
 class CallbackWebhookJob < ShopJob
   def perform_with_shop(topic:, callback:)
-    shop.rules.where(topic: topic).includes(:filters, :handlers).each do |rule|
-      next unless rule.filters.all? do |filter|
-        FilterValidator.new(filter).valid?(callback)
-      end
+    rules = shop.rules.where(topic: topic).includes(:filters, :handlers)
 
-      rule.handlers.each do |handler|
-        handler.handle(callback)
-      end
+    rules.each do |rule|
+      process_rule(rule, callback)
+    end
+  end
+
+  private
+
+  def process_rule(rule, callback)
+    return unless rule.filters.all? do |filter|
+      FilterValidator.new(filter).valid?(callback)
+    end
+
+    rule.handlers.each do |handler|
+      handler.handle(callback)
+    end
+  rescue StandardError => e
+    report_exception(e) do |report|
+      report.add_tab(:rule, {
+        rule_id: rule.id
+      })
     end
   end
 end
