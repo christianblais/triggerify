a25bf75a608bcdeff16cfe638c24da633ffea16d
Christian Blais <christ.blais@gmail.com>
2020-06-10 14:26:36 -0400

    Import rules from template

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :rules
+  resources :rules do
+    collection do
+      get :templates
+    end
+  end
 
   # OLD
   post '/webhooks/rules/:shop_id/:topic', to: 'callback#webhook', constraints: { shop_id: /\d+/, topic: /.*/ }
