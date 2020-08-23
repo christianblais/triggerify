81580197b4f4ff2c16253718c183ed4b42e49b92
Thierry Joyal <thierry.joyal@gmail.com>
2020-08-23 14:01:38 -0400

    [Emailer] Switch to sendgrid

--- app/models/handlers/emailer.rb
+++ app/models/handlers/emailer.rb
@@ -1,5 +1,7 @@
 module Handlers
   class Emailer < Base
+    class DeliveryError < StandardError; end
+
     label 'Send an email (Test only)'
 
     description %(
@@ -21,16 +23,28 @@ module Handlers
       example: 'This is an email from Triggerify!'
 
     def call
-      ActionMailer::Base.smtp_settings = {
-        :port           => ENV['MAILGUN_SMTP_PORT'],
-        :address        => ENV['MAILGUN_SMTP_SERVER'],
-        :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-        :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-        :domain         => ENV['MAILGUN_DOMAIN'],
-        :authentication => :plain,
-      }
+      mail = ::SendGrid::Mail.new
+      mail.from = ::SendGrid::Email.new(email: ENV.fetch('HANDLER_EMAILER_FROM'))
 
-      HandlerMailer.email(to: recipients, from: "triggerify@#{ENV['MAILGUN_DOMAIN']}", subject: subject, body: body).deliver_now!
+      personalization = ::SendGrid::Personalization.new
+      recipients.split(",").each do |recipient|
+        personalization.add_to(::SendGrid::Email.new(email: recipient.strip))
+      end
+      mail.add_personalization(personalization)
+
+      mail.subject = subject
+      mail.add_content(::SendGrid::Content.new(type: 'text/plain', value: body))
+
+      sg = ::SendGrid::API.new(api_key: ENV.fetch('HANDLER_EMAILER_API_KEY'))
+      response = sg.client.mail._('send').post(request_body: mail.to_json)
+
+      Rails.logger.info "Status: #{response.status_code}"
+      Rails.logger.info "Body: #{response.body}"
+      Rails.logger.info "Headers: #{response.headers}"
+
+      if response.status_code.to_i != 202
+        raise DeliveryError, "Error code: #{response.status_code}. #{response.body}"
+      end
     end
   end
 end
