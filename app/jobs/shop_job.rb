fd65203124695667feae05267ad869087fd1bfb1
Christian Blais <christ.blais@gmail.com>
2022-02-16 13:40:03 -0500

    Fix Job queuing

--- app/jobs/shop_job.rb
+++ app/jobs/shop_job.rb
@@ -3,7 +3,7 @@ class ShopJob < ApplicationJob
     @shop = Shop.find_by(shopify_domain: shop_domain)
 
     @shop.with_shopify_session do
-      perform_with_shop(args)
+      perform_with_shop(**args)
     end
   end
 
