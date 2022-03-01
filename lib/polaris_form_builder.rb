33de2316f8026f93a4f4fa50339b8cbd2202b59d
Christian Blais <christ.blais@gmail.com>
2022-03-01 16:17:54 -0500

    Polaris Select UI

--- lib/polaris_form_builder.rb
+++ lib/polaris_form_builder.rb
@@ -64,6 +64,12 @@ class PolarisFormBuilder < ActionView::Helpers::FormBuilder
   def polaris_select(method, choices, options = {}, html_options = {}, &block)
     help_text = options.delete(:help)
 
+    icon = <<-ICON
+      <svg viewBox="0 0 20 20" class="Polaris-Icon__Svg" focusable="false" aria-hidden="true">
+        <path d="M7.676 9h4.648c.563 0 .879-.603.53-1.014L10.531 5.24a.708.708 0 0 0-1.062 0L7.145 7.986C6.798 8.397 7.113 9 7.676 9zm4.648 2H7.676c-.563 0-.878.603-.53 1.014l2.323 2.746c.27.32.792.32 1.062 0l2.323-2.746c.349-.411.033-1.014-.53-1.014z"></path>
+      </svg>
+    ICON
+
     classes = ["Polaris-Select"]
     classes << "Polaris-Select--error" if @object && @object.errors.details[method.to_sym].present?
 
@@ -76,7 +82,20 @@ class PolarisFormBuilder < ActionView::Helpers::FormBuilder
         end,
         
         @template.content_tag(:div, class: classes) do
-          select(method, choices, options, html_options )
+          @template.safe_join([
+            select(method, choices, options, { class: "Polaris-Select__Input" }.merge(html_options) ),
+
+            @template.content_tag(:div, class: "Polaris-Select__Content") do
+              @template.safe_join([
+                @template.content_tag(:span, nil, class: "Polaris-Select__SelectedOption"),
+                @template.content_tag(:span, class: "Polaris-Select__Icon") do
+                  @template.content_tag(:span, @template.raw(icon), class: "Polaris-Icon")
+                end
+              ])
+            end,
+
+            @template.content_tag(:span, nil, class: "Polaris-Select__Backdrop")
+          ])
         end,
 
         if help_text
